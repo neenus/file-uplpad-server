f470cc3ea5936f78f3e774a429bf5c5743a8bf3d
Neenus Gabriel <hello@neenus.com>
2023-08-30 17:02:51 +0000

    feat(user, auth routes): update routes
    update auth routes to include logout endpoint and user routes to add requireAuth middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 // import register and login from auth controller
-import { register, login } from '../controllers/auth.controller.js';
+import { register, login, logout } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/logout').get(logout);
 
 export default router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import requireAuth from "../middlewares/auth.middleware.js";
 
 import { getUsers, getUser, updateUser } from '../controllers/users.controller.js';
 
 const router = express.Router();
 
-router.route('/').get(getUsers);
-router.route('/:id').get(getUser).put(updateUser);
+router.route('/').get(requireAuth, getUsers);
+router.route('/:id').get(requireAuth, getUser).put(requireAuth, updateUser);
 
 export default router;
